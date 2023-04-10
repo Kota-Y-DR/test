c1a5f864732770259af21e0a0a5b06751b1ecd4d
Kota-Y-dr <kota-y-dr@outlook.jp>
2023-04-10 15:50:16 +0900

    TESTING

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,21 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Post;
-/**
- * Post一覧を表示する
- * 
- * @param Post Postモデル
- * @return array Postモデルリスト
- */
-public function index(Post $post)
+
+use App\Models\Post ;
 
 {
-    return $post->get();//$postの中身を戻り値にする。
+    
 }
-
-
 class PostController extends Controller
 {
-    //
+public function index(Post $post)
+{ 
+    return $post->get();//$postの中身を戻り値にする。
 }
+}
+
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use App\Http\Controllers\PostController;
 
 Route::get('/posts', [PostController::class, 'index']);
 
+Route::get('/', function() {
+    return view('posts/index');
+});
 
 Route::get('/', function () {
     return view('welcome');
